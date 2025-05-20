ba04cd2bd629c7e8993c35ac82e59d9d163f858b
codebyyashvi <153438162+codebyyashvi@users.noreply.github.com>
2025-05-20 16:01:01 +0530

    image fixed in mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,22 +20,25 @@ const Hero = () => {
   }, [])
   return (
     <section className='relative w-full h-[160vh] mx-auto'>
-        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-          {/* Mobile image on top */}
+        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col md:flex-row items-start gap-5`}>
+          {/* Image on top for mobile */}
           {isMobile && (
             <div className='w-full flex justify-center mb-4'>
               <img src={me} alt="me" className='rounded-full w-40 h-40' />
             </div>
           )}
-          <div className='flex flex-col justify-center items-center mt-5'>
-            <div className='w-5 h-5 rounded-full bg-[#8c58fd]'/>
-            <div className='w-1 sm:h-80 h-40 violet-gradient mx-auto'/>
-          </div>
-          <div>
-            <h1 className={`${styles.heroHeadText} sm:text-[34px] text-white`}>Hi, I'm <span className='text-[#8c58fd]'>Yashvi</span></h1>
-            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop AI chatbot using Hugging <br className='sm:block hidden'/>Face API, along with mobile and <br className='sm:block hidden'/> web applications.
-            </p>
+
+          <div className='flex flex-row gap-4'>
+            <div className='flex flex-col justify-center items-center mt-5'>
+              <div className='w-5 h-5 rounded-full bg-[#8c58fd]'/>
+              <div className='w-1 sm:h-80 h-40 violet-gradient mx-auto'/>
+            </div>
+            <div>
+              <h1 className={`${styles.heroHeadText} sm:text-[34px] text-white`}>Hi, I'm <span className='text-[#8c58fd]'>Yashvi</span></h1>
+              <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                I develop AI chatbot using Hugging <br className='sm:block hidden'/>Face API, along with mobile and <br className='sm:block hidden'/> web applications.
+              </p>
+            </div>
           </div>
           {!isMobile && (<div className='mx-auto my-2'>
             <img src={me} alt="me" className='rounded-full'/>
